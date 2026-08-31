16010796db1e5862eb307bf9c71ad6f75476ba27
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-08-31 14:32:16 +0530

    Add YouTube audio download functionality

---
+++ utils/audio_processor.py
@@ -0,0 +1,35 @@
+import yt_dlp
+import os
+
+DOWNLOAD_DIR = "downloads"
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+def download_youtube_audio(url: str) -> str:
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": output_path,
+        "noplaylist": True,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+                "preferredquality": "192",
+            }
+        ],
+        "quiet": False,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+
+        original_filename = ydl.prepare_filename(info)
+        filename = os.path.splitext(original_filename)[0] + ".wav"
+
+    return filename
+
+
+url = "https://www.youtube.com/watch?v=0fB0gr_M7Pw"
+
+print(download_youtube_audio(url))
